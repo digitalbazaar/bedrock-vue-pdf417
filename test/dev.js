caf6ea12b994d5c5dba848152db23cfdbf416584
Matthew Collier <mcollier@digitalbazaar.com>
2021-02-16 14:06:43 -0500

    Refactor start script in test folder.

---
+++ test/dev.js
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const bedrock = require('bedrock');
+require('bedrock-views');
+require('bedrock-webpack');
+require('./config');
+
+bedrock.start();
