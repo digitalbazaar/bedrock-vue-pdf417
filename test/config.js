63549a30a357a1317c976c84238c2d2f5f6072b2
Dave Longley <dlongley@digitalbazaar.com>
2021-03-25 16:44:04 -0400

    Remove unnecessary webpack config.

--- test/config.js
+++ test/config.js
@@ -14,12 +14,3 @@ config.views.bundle.packages.push({
   path: path.join(__dirname, 'components'),
   manifest: path.join(__dirname, 'package.json')
 });
-
-// FIXME: can we remove this entirely now?
-config['bedrock-webpack'].configs.push({
-  node: {
-    fs: 'empty',
-    process: true,
-    Buffer: true,
-  }
-});
