b64946bbacb450ff3edc52d00d0bdc059eee590c
Josh Dunn <jdunn@digitalbazaar.com>
2022-02-28 17:57:46 -0700

    Add dynamsoft scanner.

---
+++ dbr.js
@@ -0,0 +1,4 @@
+import DBR from 'dynamsoft-javascript-barcode';
+DBR.BarcodeReader.engineResourcePath =
+  'https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@8.8.7/dist/';
+export default DBR;
