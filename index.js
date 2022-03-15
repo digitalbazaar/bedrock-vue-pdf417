6161715affe39f81395cc84fb5d5bd3d4cc76ba2
Josh Dunn <jdunn@digitalbazaar.com>
2022-03-15 14:33:33 -0600

    Remove Scanner from library.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /*!
  * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
  */
-import Scanner from './components/Scanner.vue';
 import BarcodeScanner from './components/BarcodeScanner.vue';
 
-export {Scanner, BarcodeScanner};
+export {BarcodeScanner};
