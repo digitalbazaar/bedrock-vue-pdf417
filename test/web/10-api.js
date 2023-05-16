48676b8025eae7ffb0e97a8ef72aec157f2833ed
David I. Lehn <dlehn@digitalbazaar.com>
2023-05-16 16:42:09 -0400

    Add test to ensure API loads.

---
+++ test/web/10-api.js
@@ -0,0 +1,10 @@
+/*!
+ * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
+ */
+import * as vuePdf417 from '@bedrock/vue-pdf417';
+
+describe('API', () => {
+  it('exists', () => {
+    should.exist(vuePdf417);
+  });
+});
